24da5cf76e9b97a173f147d3c0ec22dafb7674a1
Walter Ocampo <fearless347@gmail.com>
2021-01-30 18:52:41 -0500

    Add: Sección categorías en Home

--- resources/views/web/home/categories.blade.php
+++ resources/views/web/home/categories.blade.php
@@ -1,24 +1,22 @@
 <section class="section">
     <div class="container">
       <div class="has-text-centered" id="services-text-container">
-        <h1 class="title is-1">Our Services</h1>
-        <h4 class="subtitle">
-          There are many variations of passages of Lorem Ipsum available, but the majority have
-        </h4>
+        <h1 class="title is-1"> {{ __('Our Products') }} </h1>
+        <h4 class="subtitle"> {{ __('Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta, ex.') }} </h4>
       </div>
       <br />
       <div class="columns">
         <div class="column">
           <div class="card">
             <div class="card-content">
-              <div class="has-text-centered">
-                <img src="{{ asset('/assets/web/images/card-item-1.png') }}" alt="" />
+              <div class="is-flex is-justify-content-center">
+                <figure class="image is-128x128">
+                    <img src="{{ asset('/assets/web/images/logotipo.png') }}" alt="" />
+                </figure>
               </div>
-              <h3 class="title is-3 has-text-centered" id="card-product-description">Orange</h3>
+              <h3 class="title is-3 has-text-centered" id="card-product-description"> {{ __('Fresh') }} </h3>
               <p class="has-text-centered">
-                There are many variations of passages of Lorem Ipsum available, but the majority
-                have suffered alteration in some form, by injected humour, or randomised words
-                which don't look even slightly believable.
+                {{ __('Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus ratione, natus ipsam facere, temporibus dolores, aliquid nostrum repellat quaerat repudiandae at sapiente commodi officiis aut!') }}
               </p>
             </div>
           </div>
@@ -26,14 +24,14 @@
         <div class="column">
           <div class="card">
             <div class="card-content">
-              <div class="has-text-centered">
-                <img src="{{ asset('/assets/web/images/card-item-1.png') }}" alt="" />
+              <div class="is-flex is-justify-content-center">
+                <figure class="image is-128x128">
+                    <img src="{{ asset('/assets/web/images/logotipo.png') }}" alt="" />
+                </figure>
               </div>
-              <h3 class="title is-3 has-text-centered" id="card-product-description">Orange</h3>
+              <h3 class="title is-3 has-text-centered" id="card-product-description"> {{ __('Dehydrated') }} </h3>
               <p class="has-text-centered">
-                There are many variations of passages of Lorem Ipsum available, but the majority
-                have suffered alteration in some form, by injected humour, or randomised words
-                which don't look even slightly believable.
+                {{ __('Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus ratione, natus ipsam facere, temporibus dolores, aliquid nostrum repellat quaerat repudiandae at sapiente commodi officiis aut!') }}
               </p>
             </div>
           </div>
@@ -41,21 +39,21 @@
         <div class="column">
           <div class="card">
             <div class="card-content">
-              <div class="has-text-centered">
-                <img src="{{ asset('/assets/web/images/card-item-1.png') }}" alt="" />
+              <div class="is-flex is-justify-content-center">
+                <figure class="image is-128x128">
+                    <img src="{{ asset('/assets/web/images/logotipo.png') }}" alt="" />
+                </figure>
               </div>
-              <h3 class="title is-3 has-text-centered" id="card-product-description">Orange</h3>
+              <h3 class="title is-3 has-text-centered" id="card-product-description"> {{ __('Preserves') }} </h3>
               <p class="has-text-centered">
-                There are many variations of passages of Lorem Ipsum available, but the majority
-                have suffered alteration in some form, by injected humour, or randomised words
-                which don't look even slightly believable.
+                {{ __('Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus ratione, natus ipsam facere, temporibus dolores, aliquid nostrum repellat quaerat repudiandae at sapiente commodi officiis aut!') }}
               </p>
             </div>
           </div>
         </div>
       </div>
       <div class="buttons is-centered">
-        <button class="button is-black is-medium is-link">Read More</button>
+        <button class="button is-black is-button-green is-medium is-link"> {{ __('Know more') }} </button>
       </div>
     </div>
 </section>
