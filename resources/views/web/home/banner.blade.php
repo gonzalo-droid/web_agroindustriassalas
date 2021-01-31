5dba00e0b7c4a64ef75b2eab8eabc3202e7ac228
Walter Ocampo <fearless347@gmail.com>
2021-01-30 19:18:14 -0500

    Banner con efecto

--- resources/views/web/home/banner.blade.php
+++ resources/views/web/home/banner.blade.php
@@ -1,3 +1,3 @@
-<section class="section" id="tasty-fruits-section">
-  <h1 class="title is-1 is-white-text-title has-text-centered">VERY TASTY FRUITS</h1>
+<section class="section" id="banner-section">
+  <h1 class="title is-2 has-text-primary has-text-centered"> {{ __('QUALITY PRODUCTS') }} </h1>
 </section>
