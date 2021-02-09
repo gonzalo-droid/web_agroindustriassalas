0c0080c6bbbdb9a560b293b7e5ba98a1ed35437d
Walter Ocampo <fearless347@gmail.com>
2021-02-09 14:46:53 -0500

    Add: Seccion cetificados v1

---
+++ resources/views/web/home/certificates.blade.php
@@ -0,0 +1,65 @@
+<div class="section-color services py-6 my-6 mx-3 px-3">
+    <div class="container">
+      <div class="columns is-multiline">
+        <div class="column is-12 about-me" data-aos="fade-in" data-aos-easing="linear">
+          <h1 class="title has-text-centered section-title"> {{ __('Certificates') }} </h1>
+
+          <h2 class="subtitle has-text-centered">
+            {{ __('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua') }}
+          </h2>
+          <br />
+        </div>
+        <div class="columns is-12">
+          <div
+            class="column is-4 has-text-centered"
+            data-aos="fade-in"
+            data-aos-easing="linear"
+          >
+            <div class="is-flex is-justify-content-center">
+              <figure class="image is-128x128">
+                  <img src="http://localhost:8000/assets/web/images/logotipo.png" alt="">
+              </figure>
+            </div>
+            <hr />
+            <h2>
+              {{ __('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua') }}
+            </h2>
+          </div>
+          <div
+            class="column is-4 has-text-centered"
+            data-aos="fade-in"
+            data-aos-easing="linear"
+          >
+            <div class="is-flex is-justify-content-center">
+              <figure class="image is-128x128">
+                  <img src="http://localhost:8000/assets/web/images/logotipo.png" alt="">
+              </figure>
+            </div>
+            <hr />
+            <h2>
+              {{ __('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua') }}
+            </h2>
+          </div>
+          <div
+            class="column is-4 has-text-centered"
+            data-aos="fade-in"
+            data-aos-easing="linear"
+          >
+            <div class="is-flex is-justify-content-center">
+              <figure class="image is-128x128">
+                  <img src="http://localhost:8000/assets/web/images/logotipo.png" alt="">
+              </figure>
+            </div>
+            <hr />
+            <h2>
+              {{ __('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua') }}
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
