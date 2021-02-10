781a20bc29d412c8613e83c5dccc1d66cc0a547d
Walter Ocampo <fearless347@gmail.com>
2021-02-10 13:08:16 -0500

    fix: Responsive corregido

--- resources/views/web/layout/components/footer.blade.php
+++ resources/views/web/layout/components/footer.blade.php
@@ -1,85 +1,83 @@
 <footer class="footer-secondary">
-    <div class="footer-main">
-        <div class="columns">
-            <div class="column">
-                <div class="is-3 is-flex is-justify-content-center">
-                    <figure class="image is-128x128">
-                        <img src="{{ asset('/assets/web/images/logotipo.png') }}" />
-                    </figure>
-                </div>
-                <p class="has-text-justified">
-                    {{ __('Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto consequuntur ad minima impedit nemo dolorem quae at, ipsum iste ex.') }}
-                </p>
+    <div class="columns">
+        <div class="column">
+            <div class="is-3 is-flex is-justify-content-center">
+                <figure class="image is-128x128">
+                    <img src="{{ asset('/assets/web/images/logotipo.png') }}" />
+                </figure>
             </div>
-            <div class="column">
-                <h4 class="title has-text-centered is-3"> {{ __('Map website') }} </h4>
-                <div class="content is-flex is-flex-column is-justify-content-center">
-                    <ul class="footer-list">
-                        <li>
-                            <a href="#" class="navbar-item pl-0">
-                                <span class="icon is-small has-text-grey-light"><i class="fas fa-arrow-right"></i></span>
-                                <span class="pl-3"> {{ __('Inicio') }} </span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" class="navbar-item pl-0">
-                                <span class="icon is-small has-text-grey-light"><i class="fas fa-arrow-right"></i></span>
-                                <span class="pl-3"> {{ __('Nosotros') }} </span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" class="navbar-item pl-0">
-                                <span class="icon is-small has-text-grey-light"><i class="fas fa-arrow-right"></i></span>
-                                <span class="pl-3"> {{ __('Contacto') }} </span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+            <p class="has-text-justified">
+                {{ __('Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto consequuntur ad minima impedit nemo dolorem quae at, ipsum iste ex.') }}
+            </p>
+        </div>
+        <div class="column">
+            <h4 class="title has-text-centered-desktop is-3"> {{ __('Map website') }} </h4>
+            <div class="content is-flex-desktop is-flex-column is-justify-content-center">
+                <ul class="footer-list">
+                    <li>
+                        <a href="#" class="navbar-item pl-0">
+                            <span class="icon is-small has-text-grey-light"><i class="fas fa-arrow-right"></i></span>
+                            <span class="pl-3"> {{ __('Inicio') }} </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="navbar-item pl-0">
+                            <span class="icon is-small has-text-grey-light"><i class="fas fa-arrow-right"></i></span>
+                            <span class="pl-3"> {{ __('Nosotros') }} </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="navbar-item pl-0">
+                            <span class="icon is-small has-text-grey-light"><i class="fas fa-arrow-right"></i></span>
+                            <span class="pl-3"> {{ __('Contacto') }} </span>
+                        </a>
+                    </li>
+                </ul>
             </div>
-            <div class="column">
-                <h4 class="title has-text-centered is-3"> {{ __('Products') }} </h4>
-                <div class="content is-flex is-flex-column is-justify-content-center">
-                    <ul class="footer-list">
-                        <li>
-                            <a href="#" class="navbar-item pl-0">
-                                <span class="icon is-small has-text-grey-light"><i class="fas fa-arrow-right"></i></span>
-                                <span class="pl-3"> {{ __('Fresh') }} </span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" class="navbar-item pl-0">
-                                <span class="icon is-small has-text-grey-light"><i class="fas fa-arrow-right"></i></span>
-                                <span class="pl-3"> {{ __('Dehydrated') }} </span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" class="navbar-item pl-0">
-                                <span class="icon is-small has-text-grey-light"><i class="fas fa-arrow-right"></i></span>
-                                <span class="pl-3"> {{ __('Preserves') }} </span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+        </div>
+        <div class="column">
+            <h4 class="title has-text-centered-desktop is-3"> {{ __('Products') }} </h4>
+            <div class="content is-flex-desktop is-flex-column is-justify-content-center">
+                <ul class="footer-list">
+                    <li>
+                        <a href="#" class="navbar-item pl-0">
+                            <span class="icon is-small has-text-grey-light"><i class="fas fa-arrow-right"></i></span>
+                            <span class="pl-3"> {{ __('Fresh') }} </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="navbar-item pl-0">
+                            <span class="icon is-small has-text-grey-light"><i class="fas fa-arrow-right"></i></span>
+                            <span class="pl-3"> {{ __('Dehydrated') }} </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="navbar-item pl-0">
+                            <span class="icon is-small has-text-grey-light"><i class="fas fa-arrow-right"></i></span>
+                            <span class="pl-3"> {{ __('Preserves') }} </span>
+                        </a>
+                    </li>
+                </ul>
             </div>
+        </div>
 
-            <div class="column">
-                <h4 class="title has-text-centered is-3"> {{ __('Follow Us') }} </h4>
-                <div class="buttons is-flex is-justify-content-center">
-                    <a href="#" class="button is-medium is-facebook is-primary-social">
-                        <span class="icon">
-                            <i class="fab fa-facebook fa-lg"></i>
-                        </span>
-                    </a>
-                    <a href="#" class="button is-medium is-instagram is-primary-social">
-                        <span class="icon">
-                            <i class="fab fa-instagram fa-lg"></i>
-                        </span>
-                    </a>
-                </div>
+        <div class="column">
+            <h4 class="title has-text-centered-desktop is-3"> {{ __('Follow Us') }} </h4>
+            <div class="is-flex-desktop is-justify-content-center">
+                <a href="#" class="button mx-1 is-medium is-facebook is-primary-social">
+                    <span class="icon">
+                        <i class="fab fa-facebook fa-lg"></i>
+                    </span>
+                </a>
+                <a href="#" class="button mx-1 is-medium is-instagram is-primary-social">
+                    <span class="icon">
+                        <i class="fab fa-instagram fa-lg"></i>
+                    </span>
+                </a>
             </div>
         </div>
     </div>
-    <div class="content has-text-centered pt-6 mt-6">
+    <div class="content has-text-centered mt-6">
         <p class="has-text-dark">
             {{ __('Copyright © 2021 | AgroIndustrias Salas | By') }}
             <strong>
