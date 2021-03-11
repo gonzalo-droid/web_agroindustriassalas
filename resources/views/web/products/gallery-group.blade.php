b74e5f799ef2c9a3df1af5679d67776a800c8248
Walter Ocampo <fearless347@gmail.com>
2021-03-11 10:50:43 -0500

    cards horizontales

--- resources/views/web/products/gallery-group.blade.php
+++ resources/views/web/products/gallery-group.blade.php
@@ -2,7 +2,7 @@
     <div class="has-text-centered py-6">
 
         <div class="columns">
-            <div class="column">
+            <div class="column container">
                 <h1 class="title is-2">
                     {{ __('Products') }}
                 </h1>
@@ -10,9 +10,9 @@
                     {{ __('Lorem ipsum dolor sit amet.') }}
                 </h2>
                 <br>
-                <div class=" grid-cols-3 gap-4 flex space-x-4">
+                <div class="columns">
                     
-                    <div class="card flex-1">
+                    <div class="card column mx-4">
                         <div class="card-content">
                           <div class="is-flex is-justify-content-center">
                             <i class="fab fa-pagelines" style="font-size: 50px"></i>
@@ -20,7 +20,7 @@
                           <h5 class="title is-4 has-text-centered" id="card-product-description"> {{ __('Fresh') }} </h5>                          
                         </div>
                       </div>
-                    <div class="card flex-1">
+                    <div class="card column mx-4">
                       <div class="card-content">
                         <div class="is-flex is-justify-content-center">
                           <i class="fab fa-pagelines" style="font-size: 50px"></i>
@@ -31,7 +31,7 @@
                     </div>
                   
                  
-                    <div class="card flex-1 ">
+                    <div class="card column mx-4 ">
                       <div class="card-content">
                         <div class="is-flex is-justify-content-center">
                           <i class="fab fa-pagelines" style="font-size: 50px"></i>
@@ -40,7 +40,7 @@
                         
                       </div>
                     </div>
-                    <div class="card flex-1">
+                    <div class="card column mx-4">
                         <div class="card-content">
                           <div class="is-flex is-justify-content-center">
                             <i class="fab fa-pagelines" style="font-size: 50px"></i>
